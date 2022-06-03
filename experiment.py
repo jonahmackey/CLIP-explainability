663cd4e3bda70aa4cfbd5397916806c6f26ac8a5
Jonah Mackey <jonahmackey699@gmail.com>
2022-06-03 11:08:06 -0400

    Experiment code updated

--- experiment.py
+++ experiment.py
@@ -27,6 +27,7 @@ sv_index_lst = [
 threshold_lst = [
     0.1,
     0.05,
+    0.001
 ]
 
 freq_reg_lst = [
@@ -34,52 +35,44 @@ freq_reg_lst = [
     "norm"
 ]
 
-lr_lst = [
-    1,
-    250,
-    500,
-    750
-]
-
 for img_fp in img_lst:
     i = 0
     for sv_index in sv_index_lst:
         for threshold in threshold_lst:
             for freq_reg in freq_reg_lst:
-                for lr in lr_lst:
-                    exp_name = img_fp[9:-4]
-                    
-                    save_path = "./Results/" + exp_name + f"{i}" # datetime.now().strftime("%y%m%d-%H%M%S-") + exp_name
+                exp_name = img_fp[9:-4]
+                
+                save_path = "./Results/" + exp_name + f"{i}" # datetime.now().strftime("%y%m%d-%H%M%S-") + exp_name
 
-                    clip_dream(img_fp=img_fp,
-                                save_path=save_path,
-                                theta=1,
-                                sv_index=sv_index,
-                                rand_dir=False,
-                                threshold=threshold, 
-                                eps=1e-6,
-                                num_cutouts=32,
-                                freq_reg=freq_reg,
-                                lr=lr,
-                                max_iters=5000)
-                    
-                    file = open(save_path + "/myfile.txt", "w")
-                    params = f"Img encoder: {'ViT-B/32'}\n" \
-                        + f"img_fp: {img_fp}\n" \
-                        + f"save_path: {save_path}\n" \
-                        + f"theta: {1}\n" \
-                        + f"sv_index {sv_index}\n" \
-                        + f"rand_dir: {False}\n" \
-                        + f"threshold: {threshold}\n" \
-                        + f"eps: {1e-6}\n" \
-                        + f"num_cutouts: {32}\n" \
-                        + f"freq_reg: {freq_reg}\n" \
-                        + f"lr: {lr}\n" \
-                        + f"max_iters: {3000}"
-                    _ = file.write(params)
-                    file.close()
-                    
-                    visualize_dream_loss(data_fp=save_path + "/experiment_data.csv", threshold=threshold, save_path=save_path)
-                    
-                    i += 1
-                    
+                clip_dream(img_fp=img_fp,
+                            save_path=save_path,
+                            theta=1,
+                            sv_index=sv_index,
+                            rand_dir=False,
+                            threshold=threshold, 
+                            eps=1e-6,
+                            num_cutouts=32,
+                            freq_reg=freq_reg,
+                            lr=250,
+                            max_iters=5000)
+                
+                file = open(save_path + "/myfile.txt", "w")
+                params = f"Img encoder: {'ViT-B/32'}\n" \
+                    + f"img_fp: {img_fp}\n" \
+                    + f"save_path: {save_path}\n" \
+                    + f"theta: {1}\n" \
+                    + f"sv_index {sv_index}\n" \
+                    + f"rand_dir: {False}\n" \
+                    + f"threshold: {threshold}\n" \
+                    + f"eps: {1e-6}\n" \
+                    + f"num_cutouts: {32}\n" \
+                    + f"freq_reg: {freq_reg}\n" \
+                    + f"lr: {250}\n" \
+                    + f"max_iters: {3000}"
+                _ = file.write(params)
+                file.close()
+                
+                visualize_dream_loss(data_fp=save_path + "/experiment_data.csv", threshold=threshold, save_path=save_path)
+                
+                i += 1
+                
